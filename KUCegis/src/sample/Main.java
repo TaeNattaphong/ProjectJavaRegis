ba4d8b25f5ea04a163fbdd7fb64b378e27a775a2
Unknown <nattaphong.r@ku.th>
2018-11-03 16:11:21 +0700

    Change Title Program

--- KUCegis/src/sample/Main.java
+++ KUCegis/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        primaryStage.setTitle("KUCegiS");
+        primaryStage.setTitle("KUCegiSsssssssss");
         primaryStage.setScene(new Scene(root, 337, 322));
         primaryStage.setResizable(false);
         primaryStage.show();
