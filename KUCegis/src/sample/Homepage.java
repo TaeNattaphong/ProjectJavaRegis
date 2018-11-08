b387765ffde961f944f8803b22a0d8bf13eefa8c
boss <parinya.thon@ku.th>
2018-11-08 15:57:00 +0700

    Homepage2

--- KUCegis/src/sample/Homepage.java
+++ KUCegis/src/sample/Homepage.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
+
 public class Homepage {
+    @FXML
+    private  Label LabelName;
+
+    @FXML
+    public void handdleButton(ActionEvent event){
+        LabelName.setText("Hello");
+    }
 }
