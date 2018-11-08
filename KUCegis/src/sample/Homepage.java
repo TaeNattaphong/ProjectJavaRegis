c2a702aa22908ae4c625851fd47991fdbd64c62c
boss <parinya.thon@ku.th>
2018-11-08 18:09:24 +0700

    Update Homepage

--- KUCegis/src/sample/Homepage.java
+++ KUCegis/src/sample/Homepage.java
@@ -2,8 +2,9 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-
+import javafx.scene.layout.VBox;
 
 
 public class Homepage {
@@ -13,5 +14,14 @@ public class Homepage {
     @FXML
     public void handdleButton(ActionEvent event){
         LabelName.setText("Hello");
+    }
+
+    @FXML private VBox addBottonVbox;
+
+    @FXML
+    public void initialize() {
+        for (int i=0; i<100; i++) {
+            addBottonVbox.getChildren().add(new Button(i + ""));
+        }
     }
 }
